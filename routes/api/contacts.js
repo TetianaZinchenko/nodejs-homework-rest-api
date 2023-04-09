792f76a844d9e4d9dc0e9ece88238a2b66b17d71
TetianaZinchenko <106535030+TetianaZinchenko@users.noreply.github.com>
2023-04-09 17:41:04 +0300

    fixed

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -9,9 +9,12 @@ const { HttpError } = require('../../helpers');
 const router = express.Router();
 
 const addSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
+  // name: Joi.string().required(),
+  // email: Joi.string().required(),
+  // phone: Joi.string().required(),
+  name: Joi.string(),
+  email: Joi.string(),
+  phone: Joi.string(),
 });
 
 router.get('/', async (req, res, next) => {
