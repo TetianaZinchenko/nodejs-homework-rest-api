339b7ce3bf6d127959709a2be387e8ff5366b6af
TetianaZinchenko <106535030+TetianaZinchenko@users.noreply.github.com>
2023-04-17 00:06:11 +0300

    HW3

---
+++ models/contact.js
@@ -0,0 +1,61 @@
+const { Schema, model } = require('mongoose');
+
+const Joi = require('joi');
+
+const { handleMongooseError } = require('../helpers');
+
+const nameRegExp = /^[a-zA-Z0-9]+(\s[a-zA-Z0-9]+)?$/;
+const phoneRegExp = /^(\(\d{3}\)\s*\d{3}-\d{4}|\d{5,15})$/;
+const statusOptions = [true, false];
+
+const addSchema = Joi.object({
+  name: Joi.string().pattern(nameRegExp).required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().pattern(phoneRegExp).required(),
+  favorite: Joi.boolean().valid(...statusOptions),
+});
+
+const updateSchema = Joi.object({
+  name: Joi.string().pattern(nameRegExp),
+  email: Joi.string().email(),
+  phone: Joi.string().pattern(phoneRegExp),
+  favorite: Joi.boolean().valid(...statusOptions),
+}).or('name', 'email', 'phone', 'favorite');
+
+const updateStatusSchema = Joi.object({
+  favorite: Joi.boolean().valid(...statusOptions),
+});
+
+const schemas = { addSchema, updateSchema, updateStatusSchema };
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Set <name> for contact'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Set <email> for contact'],
+    },
+    phone: {
+      type: String,
+      required: [true, 'Set <phone> for contact'],
+    },
+    favorite: {
+      type: Boolean,
+      enum: statusOptions,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+contactSchema.post('save', handleMongooseError);
+
+const Contact = model('contact', contactSchema);
+
+module.exports = {
+  Contact,
+  schemas,
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,95 +1,29 @@
 const express = require('express');
 
-const Joi = require('joi');
-
-const contacts = require('../../models/contacts');
-
-const { HttpError } = require('../../helpers');
-
 const router = express.Router();
 
-const addSchema = Joi.object({
-  name: Joi.string()
-    .pattern(/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/)
-    .required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string()
-    .pattern(/\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/)
-    .required(),
-});
+const ctrl = require('../../controllers/contacts');
 
-const updateSchema = Joi.object({
-  name: Joi.string().pattern(/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/),
-  email: Joi.string().email(),
-  phone: Joi.string().pattern(
-    /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/
-  ),
-});
+const { validateBody, isValidId } = require('../../middlewares');
 
-router.get('/', async (req, res, next) => {
-  try {
-    const result = await contacts.listContacts();
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+// const schemas = require('../../schemas/contacts');
+const { schemas } = require('../../models/contact');
 
-router.get('/:contactId', async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contacts.getContactById(contactId);
-    if (!result) {
-      throw HttpError(404, 'Not found');
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.get('/', ctrl.getAll);
 
-router.post('/', async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const result = await contacts.addContact(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.get('/:contactId', isValidId, ctrl.getById);
 
-router.delete('/:contactId', async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contacts.removeContact(contactId);
-    if (!result) {
-      throw HttpError(404, 'Not found');
-    }
-    res.json({
-      message: 'Delete success',
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.post('/', validateBody(schemas.addSchema), ctrl.add);
 
-router.put('/:contactId', async (req, res, next) => {
-  try {
-    const { error } = updateSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const result = await contacts.updateContact(req.params.contactId, req.body);
-    if (!result) {
-      throw HttpError(404, 'Not found');
-    }
-    res.status(200).json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.delete('/:contactId', isValidId, ctrl.deleteById);
+
+router.put('/:contactId', isValidId, validateBody(schemas.updateSchema), ctrl.updateById);
+
+router.patch(
+  '/:contactId/favorite',
+  isValidId,
+  validateBody(schemas.updateStatusSchema),
+  ctrl.updateStatusContact
+);
 
 module.exports = router;
