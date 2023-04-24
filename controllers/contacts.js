e1002afb18a0ecff066e42b14bf35106181a971f
TetianaZinchenko <106535030+TetianaZinchenko@users.noreply.github.com>
2023-04-25 00:14:09 +0300

    fixed

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -29,10 +29,22 @@ const add = async (req, res) => {
   res.status(201).json(result);
 };
 
-const updateById = async (req, res) => {
+// const updateById = async (req, res) => {
+//   const { _id: owner } = req.user;
+//   const { contactId } = req.params;
+//   const result = await Contact.findByIdAndUpdate({ contactId, owner }, req.body, { new: true });
+//   if (!result) {
+//     throw HttpError(404, 'Not found');
+//   }
+//   res.status(200).json(result);
+// };
+
+const updateById = async (req, res, next) => {
   const { _id: owner } = req.user;
   const { contactId } = req.params;
-  const result = await Contact.findByIdAndUpdate({ contactId, owner }, req.body, { new: true });
+  const result = await Contact.findOneAndUpdate({ _id: contactId, owner }, req.body, {
+    new: true,
+  });
   if (!result) {
     throw HttpError(404, 'Not found');
   }
